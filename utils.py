c8ceca8d39ad23a3c1d5c8b015beea4237f87d61
Tommaso <tommaso.barletti@gmail.com>
2021-09-21 16:06:23 +0200

    Added some utils function

--- utils.py
+++ utils.py
@@ -1,19 +1,23 @@
 from PIL import Image
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_agg import FigureCanvasAgg
+import plotly.express as px
 import numpy as np
 import torch
+import pandas as pd
 
-def create_features_plot(np_features, np_labels):
+def create_features_plot_matplot(np_features, np_labels, class_names):
     '''
     This function create a scatter plot of bidimensional features diffeentiating per class.
 
     Parameters
     ----------
-    features : numpy array
+    np_features : numpy array
         bx2 matrix of b samples of dimensionality 2.
-    labels : numpy array
+    np_labels : numpy array
         bx1 matrix of b labels of dimensionality 1.
+    class_names : dict
+        dict with class names associated to class index
     Returns
         PIL image
     -------
@@ -28,14 +32,20 @@ def create_features_plot(np_features, np_labels):
     if torch.is_tensor(labels):
         np_labels = labels.cpu().detach().numpy()
     '''
-    classes = np.unique(np_labels)
+    classes = np.unique(np_labels, sorted=True)
+    #print("CLASSI LEGENDA:", classes)
+
+    fig = plt.figure()
 
     for i in range(classes.shape[0]):
         indices = np.where(np_labels==classes[i])
         x1 = np_features[indices,0]
         x2 = np_features[indices,1]
         color = colors[classes[i]]
-        label = "Class-"+str(classes[i])
+        if(class_names != None):
+            label = class_names[i]
+        else:
+            label = "Class-"+str(classes[i])
         plt.scatter(x1,x2,color=color, marker= '*', label=label)
 
 
@@ -44,22 +54,32 @@ def create_features_plot(np_features, np_labels):
     plt.xlabel('Feature 1')
     plt.ylabel('Feature 2')
     plt.legend(loc='best')
+    plt.grid()
+    
+    return fig
 
-    canvas = plt.gcf().canvas
-
-    agg = canvas.switch_backends(FigureCanvasAgg)
-    agg.draw()
-    X = np.asarray(agg.buffer_rgba())
-
-    im = Image.fromarray(X)
-    return im
-
-    #plt.show()
+def create_features_plot_plotly(np_features, np_labels, class_names):
+    df = pd.DataFrame(np_features, columns = ['Feature1', 'Feature2'])
+    df["Class_name"] = [class_names[x.item()] for x in np_labels]
+    fig = px.scatter(df, x="Feature1", y="Feature2", color="Class_name")
+    return fig
 
 if __name__ == "__main__":
 
     feat = torch.rand((10,2))
     labels = torch.randint(0,3,(10,1))
+    class_names = {
+        0:"T-shirt",
+        1:"Trousers",
+        2:"Pullover",
+        3:"Dress",
+        4:"Coat",
+        5:"Sandal",
+        6:"Shirt",
+        7:"Sneaker",
+        8:"Bag",
+        9:"Ankle boot"
+    }
 
-    im = create_features_plot(feat, labels)
-    print(im)
+    im = create_features_plot_matplot(feat, labels, class_names)
+    print(type(im))
