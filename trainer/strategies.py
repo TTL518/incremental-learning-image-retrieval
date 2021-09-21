9268ccc981af5e44b89412ba28ce4c080f59d40a
Tommaso <tommaso.barletti@gmail.com>
2021-09-21 16:15:49 +0200

    Added new Naive Strategies

--- trainer/strategies.py
+++ trainer/strategies.py
@@ -5,10 +5,10 @@ from torch.optim import Optimizer
 from trainer.plugins import ILFGIR_plugin
 
 from avalanche.training.plugins.evaluation import default_logger
-from avalanche.training.plugins import StrategyPlugin, EvaluationPlugin
+from avalanche.training.plugins import StrategyPlugin, EvaluationPlugin, LwFPlugin
 from avalanche.training.strategies import BaseStrategy, Naive
 
-from trainer.plugins import ILFGIR_plugin
+from trainer.plugins import ILFGIR_plugin, Naive_plugin, ILFGIR_plugin_new
 
 class ILFGIR_strategy(BaseStrategy):
     def __init__(self, model: Module, optimizer: Optimizer, criterion,
@@ -22,6 +22,26 @@ class ILFGIR_strategy(BaseStrategy):
             plugins = [ILFGIR]
         else:
             plugins.append(ILFGIR)
+
+        super().__init__(
+            model, optimizer, criterion,
+            train_mb_size=train_mb_size, train_epochs=train_epochs,
+            eval_mb_size=eval_mb_size, device=device, plugins=plugins,
+            evaluator=evaluator, eval_every=eval_every)
+
+class Naive_strategy(BaseStrategy):
+    def __init__(self, model: Module, optimizer: Optimizer,
+                 criterion=CrossEntropyLoss(),
+                 train_mb_size: int = 1, train_epochs: int = 1,
+                 eval_mb_size: int = None, device=None,
+                 plugins: Optional[List[StrategyPlugin]] = None,
+                 evaluator: EvaluationPlugin = default_logger, eval_every=-1):
+
+        naive = Naive_plugin()
+        if plugins is None:
+            plugins = [naive]
+        else:
+            plugins.append(naive)
 
         super().__init__(
             model, optimizer, criterion,
