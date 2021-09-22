10cbc2c553a5269a7d9205c657e05c7ab0cf7679
Tommaso <tommaso.barletti@gmail.com>
2021-09-22 18:09:46 +0200

    Added LeNet classic

--- models/LeNet.py
+++ models/LeNet.py
@@ -103,8 +103,34 @@ class LeNet_PP(nn.Module):
 
         return y, x
 
+class LeNet(nn.Module):
+
+    def __init__(self, initial_num_classes, bias_classifier, norm_classifier):
+        super(LeNet, self).__init__()
+        # 1 input image channel, 6 output channels, 5x5 square convolution
+        # kernel
+        self.conv1 = nn.Conv2d(1, 6, 5)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        # an affine operation: y = Wx + b
+        self.fc1 = nn.Linear(16 * 4 * 4, 120)  # 4*4 from image dimension (28x28)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+        self.inc_classifier = NewIncrementalClassifier(84, initial_num_classes, bias_classifier, norm_classifier)
+
+    def forward(self, x):
+        # Max pooling over a (2, 2) window
+        x = F.max_pool2d(F.relu(self.conv1(x)), (2, 2))
+        # If the size is a square, you can specify with a single number
+        x = F.max_pool2d(F.relu(self.conv2(x)), 2)
+        x = torch.flatten(x, 1) # flatten all dimensions except the batch dimension
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        #y = self.fc3(x)
+        y = self.inc_classifier(x)
+        return y, x
+
 def test():
-    net = LeNet_PP(2)
+    net = LeNet(2, False, True)
     features, y = net(torch.randn(1, 1, 28, 28))
     print(y.size())
     print(features.size())
