2ccf8bc42d8c72e98cf2b5a6fdfe5f6539047f6e
tommaso <tommaso.barletti@gmail.com>
2021-10-15 15:41:29 +0200

    Added coral loss, and new implementation of existing loss

--- models/loss.py
+++ models/loss.py
@@ -39,7 +39,7 @@ def kd_loss(n_img, out, prev_out, T=2):
     
     return res
 
-def DistillationLoss(logits_a, logits_b, temperature=1.):
+def DistillationLoss(logits_a, logits_b, temperature=2.):
     return F.binary_cross_entropy_with_logits(
         torch.sigmoid(logits_a / temperature), 
         torch.sigmoid(logits_b / temperature)
@@ -219,6 +219,114 @@ class MMD_loss(nn.Module):
 
     # def MMD(a, b):
     #     return gaussian_kernel(a, a).mean() + gaussian_kernel(b, b).mean() - 2*gaussian_kernel(a, b).mean()
+
+
+class MMDStatistic:
+    r"""The *unbiased* MMD test of :cite:`gretton2012kernel`.
+    The kernel used is equal to:
+    .. math ::
+        k(x, x') = \sum_{j=1}^k e^{-\alpha_j\|x - x'\|^2},
+    for the :math:`\alpha_j` proved in :py:meth:`~.MMDStatistic.__call__`.
+    Arguments
+    ---------
+    n_1: int
+        The number of points in the first sample.
+    n_2: int
+        The number of points in the second sample."""
+
+    def __init__(self, n_1, n_2):
+        self.n_1 = n_1
+        self.n_2 = n_2
+
+        # The three constants used in the test.
+        self.a00 = 1. / (n_1 * (n_1 - 1))
+        self.a11 = 1. / (n_2 * (n_2 - 1))
+        self.a01 = - 1. / (n_1 * n_2)
+
+    def __call__(self, sample_1, sample_2, alphas, ret_matrix=False):
+        r"""Evaluate the statistic.
+        The kernel used is
+        .. math::
+            k(x, x') = \sum_{j=1}^k e^{-\alpha_j \|x - x'\|^2},
+        for the provided ``alphas``.
+        Arguments
+        ---------
+        sample_1: :class:`torch:torch.autograd.Variable`
+            The first sample, of size ``(n_1, d)``.
+        sample_2: variable of shape (n_2, d)
+            The second sample, of size ``(n_2, d)``.
+        alphas : list of :class:`float`
+            The kernel parameters.
+        ret_matrix: bool
+            If set, the call with also return a second variable.
+            This variable can be then used to compute a p-value using
+            :py:meth:`~.MMDStatistic.pval`.
+        Returns
+        -------
+        :class:`float`
+            The test statistic.
+        :class:`torch:torch.autograd.Variable`
+            Returned only if ``ret_matrix`` was set to true."""
+        sample_12 = torch.cat((sample_1, sample_2), 0)
+        distances = pdist(sample_12, sample_12, norm=2)
+
+        kernels = None
+        for alpha in alphas:
+            kernels_a = torch.exp(- alpha * distances ** 2)
+            if kernels is None:
+                kernels = kernels_a
+            else:
+                kernels = kernels + kernels_a
+
+        k_1 = kernels[:self.n_1, :self.n_1]
+        k_2 = kernels[self.n_1:, self.n_1:]
+        k_12 = kernels[:self.n_1, self.n_1:]
+
+        mmd = (2 * self.a01 * k_12.sum() +
+               self.a00 * (k_1.sum() - torch.trace(k_1)) +
+               self.a11 * (k_2.sum() - torch.trace(k_2)))
+        if ret_matrix:
+            return mmd, kernels
+        else:
+            return mmd
+
+
+
+
+def coral(source, target):
+
+    d = source.size(1)  # dim vector
+
+    source_c = compute_covariance(source)
+    target_c = compute_covariance(target)
+
+    loss = torch.sum(torch.mul((source_c - target_c), (source_c - target_c)))
+
+    loss = loss / (4 * d * d)
+    return loss
+
+
+def compute_covariance(input_data):
+    """
+    Compute Covariance matrix of the input data
+    """
+    n = input_data.size(0)  # batch_size
+
+    # Check if using gpu or cpu
+    if input_data.is_cuda:
+        device = torch.device('cuda')
+    else:
+        device = torch.device('cpu')
+
+    id_row = torch.ones(n).resize(1, n).to(device=device)
+    sum_column = torch.mm(id_row, input_data)
+    mean_column = torch.div(sum_column, n)
+    term_mul_2 = torch.mm(mean_column.t(), mean_column)
+    d_t_d = torch.mm(input_data.t(), input_data)
+    c = torch.add(d_t_d, (-1 * term_mul_2)) * 1 / (n - 1)
+
+    return c
+
 
 if __name__ == "__main__":
     a = torch.tensor([[2.1, 100.], [1.012, 3.11], [1., 3]])
