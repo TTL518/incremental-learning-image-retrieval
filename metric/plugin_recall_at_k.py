625488e4fda19f8a544585a46ae9b3ed3da774de
tommaso <tommaso.barletti@gmail.com>
2021-10-19 11:49:24 +0200

    Add checkpoint func based on recall

--- metric/plugin_recall_at_k.py
+++ metric/plugin_recall_at_k.py
@@ -240,6 +240,111 @@ class RecallAtKPluginAllTasks(PluginMetric[float]):
         name = "Recall@"+"1"+"_Exp"
         return name
 
+class RecallAtKPluginAllTasksAndCheckpoint(PluginMetric[float]):
+    """
+    This metric plugin will return a `float` value at the end of the last experience evaluation, let to calculate recall@k on all tasks all toghether (passed+actual)
+    """
+    def __init__(self, bestModelPath):
+        """
+        Initialize the metric
+        """
+        super().__init__()
+        self._NN = NearestNeighborDistanceMetric("cosine")
+        self._recall_AT_1= RecallAtK(k=1)
+        self.out_flattened_features = []
+        self.y = []
+        # current x values for the metric curve
+        self.x_coord = 0
+        self.finalEval = False
+
+        self.bestMetricValue = 0.0
+        self.bestModelPath = bestModelPath
+
+    def reset(self) -> None:
+        """
+        Reset the metric
+        """
+        self.out_flattened_features = []
+        self.y = []
+        self._recall_AT_1.reset()
+
+    def result(self) -> float:
+        """
+        Emit the result
+        """
+        return self._recall_AT_1.result()
+
+    def before_eval(self, strategy: 'PluggableStrategy') -> None:
+        """
+        Reset the recall@k before the evaluation phase
+        """
+        self.reset()
+        if(strategy.epoch == strategy.train_epochs):
+            self.finalEval=True
+            print("Evaluation finale")
+        else:
+            print("Evaluation durante training")
+            self.finalEval=False
+
+    def before_training_exp(self, strategy: 'PluggableStrategy'):
+        print("Azzero best metric value per salvare modello migliore durante training")
+        self.bestMetricValue = 0.0
+
+    def after_eval_iteration(self, strategy: 'PluggableStrategy') -> None:
+        """
+        Get features of batch test images
+        """
+        #if self.finalEval:
+        
+        if(strategy.plugins[0].mb_out_flattened_features.is_cuda or strategy.mb_y.is_cuda):
+            mb_out_flattened_features = strategy.plugins[0].mb_out_flattened_features.detach().cpu()
+            labels = strategy.mb_y.detach().cpu()
+            self.out_flattened_features.append(mb_out_flattened_features)
+            self.y.append(labels) 
+
+    def after_eval(self, strategy: 'PluggableStrategy') -> 'MetricResult':
+        """
+        Emit the result at the end of all experiences
+        """
+            
+        features = torch.cat(self.out_flattened_features)
+        y = torch.cat(self.y)
+        features = torch.squeeze(features)
+        y = torch.squeeze(y)
+
+        features = normalize(features)
+
+        distances_matrix = self._NN.distance(features, features)
+        self._recall_AT_1.update(distances_matrix, y, y, True )
+        value = self.result()
+        
+        self.x_coord += 1 #strategy.epoch # increment x value
+        name = "Recall@"+"1"+"_All_Exp"
+
+        if(self.finalEval==False): #Controllo che non sia l'evaluation FINALE (a fine training)
+            value = self.result()
+            print("Recall@1: ", value)
+            print("Best Recall@1: ", self.bestMetricValue)
+            if value > self.bestMetricValue:
+                print("Salvo nuovo miglior modello")
+                best_dict= {
+                    'training_exp':strategy.experience.current_experience, 
+                    'training_epoch': strategy.epoch,
+                    'model_state_dict': strategy.model.state_dict(),
+                    'optimizer_state_dict': strategy.optimizer.state_dict()
+                }
+                self.bestMetricValue = value
+                torch.save(best_dict, self.bestModelPath)
+        
+        return [MetricValue(self, name, value, self.x_coord)]
+
+    def __str__(self):
+        """
+        Here you can specify the name of your metric
+        """
+        name = "Recall@"+"1"+"_Exp"
+        return name
+
 
 if __name__ == "__main__":
     metric = RecallAtK()
